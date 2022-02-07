085097237440b4007aed60c2bd620abef7519f52
Igor Radovic <igor9levi@gmail.com>
2022-02-07 18:50:22 +0100

    fix nftcard stories

--- src/components/nftCard/NFTCard.stories.tsx
+++ src/components/nftCard/NFTCard.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import NFTCard from './NFTCard';
-import { TokenType } from '../../types';
 
 export default {
   title: 'Components/NFTCard',
