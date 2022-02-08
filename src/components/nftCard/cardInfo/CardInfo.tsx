02145f29f8dd3aebd592f6380f4b2c9b05893dbe
Igor Radovic <igor9levi@gmail.com>
2022-02-09 00:46:06 +0100

    add button to cardInfo

--- src/components/nftCard/cardInfo/CardInfo.tsx
+++ src/components/nftCard/cardInfo/CardInfo.tsx
@@ -9,6 +9,7 @@ import styles from './cardInfo.module.scss';
 
 // Utils
 import { outboundLinkBuilder } from '../../../utils';
+import Button from '../../../common/button';
 
 interface CardInfoProps {
   content: IToken;
@@ -18,23 +19,32 @@ const CardInfo = ({ content }: CardInfoProps): JSX.Element => {
   const { tokenId, contractAddress } = content;
 
   return (
-    <div className={styles.cardInfo}>
-      <div className={styles.tokenIdInfo}>
-        <div className={styles.tokenIdLabel}>Token ID</div>
-        <Text className={styles.tokenId}>{tokenId}</Text>
-      </div>
-      <div className={styles.tokenAddressInfo}>
-        <Text className={styles.tokenLabel}>Token Address</Text>
-        <div className={styles.tokenAddress}>
-          <a
-            target="_blank"
-            href={outboundLinkBuilder(contractAddress)}
-            rel="noreferrer"
-          >
-            {contractAddress}
-          </a>
+    <div className={styles.container}>
+      <div className={styles.cardInfo}>
+        <div className={styles.tokenIdInfo}>
+          <div className={styles.tokenIdLabel}>Token ID</div>
+          <Text className={styles.tokenId}>{tokenId}</Text>
+        </div>
+        <div className={styles.tokenAddressInfo}>
+          <Text className={styles.tokenLabel}>Token Address</Text>
+          <div className={styles.tokenAddress}>
+            <a
+              target="_blank"
+              href={outboundLinkBuilder(contractAddress)}
+              rel="noreferrer"
+            >
+              {contractAddress}
+            </a>
+          </div>
         </div>
       </div>
+      <Button
+        onClick={() => {
+          /* noop */
+        }}
+      >
+        Transfer NFT to another account
+      </Button>
     </div>
   );
 };
