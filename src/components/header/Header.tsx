e29f2c0e5b2bbb6d8d3cc2fece46774e130bc4d2
Igor Radovic <igor9levi@gmail.com>
2022-02-10 05:56:58 +0100

    fix lint

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { injected } from '../../connectors';
 
